51674d51a0921d72c29263ebaa1bf1249d3f8dff
grcbrown <122645861+grcbrown@users.noreply.github.com>
2025-10-18 20:25:11 -0700

    trial data bug fixes

--- experiment/util.js
+++ experiment/util.js
@@ -2,8 +2,8 @@ function create_tv_array(json_object) {
     let tv_array = [];
     for (let i = 0; i < json_object.length; i++) {
         obj = {};
+        obj.stimulus = json_object[i].stimulus;
         obj.data = {};
-        obj.data.stimulus = json_object[i].stimulus;
         obj.data.id = json_object[i].id;
         tv_array.push(obj)
     }
