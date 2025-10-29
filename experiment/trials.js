7c74e41c8694be30f2797771feb40196808cdb9b
grcbrown <122645861+grcbrown@users.noreply.github.com>
2025-10-28 20:58:39 -0700

    cosmetic updates

--- experiment/trials.js
+++ experiment/trials.js
@@ -1,34 +1,34 @@
 let trial_objects = [
     {
         "stimulus": "audio/330_705_B4.wav",
-        "id": 330,
+        "id": 330
     },
     {
         "stimulus": "audio/493_705_B4.wav",
-        "id": 493,
+        "id": 493
     },
     {
         "stimulus": "audio/516_705_B4.wav",
-        "id": 516,
+        "id": 516
     },
     {
         "stimulus": "audio/573_705_B4.wav",
-        "id": 573,
+        "id": 573
     },
     {
         "stimulus": "audio/672_705_B4.wav",
-        "id": 672,
+        "id": 672
     },
     {
         "stimulus": "audio/752_705_B4.wav",
-        "id": 752,
+        "id": 752
     },
     {
         "stimulus": "audio/799_705_B4.wav",
-        "id": 799,
+        "id": 799
     },
     {
         "stimulus": "audio/955_705_B4.wav",
-        "id": 955,
+        "id": 955
     }
 ]
